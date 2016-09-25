a7e63e28524c5aca3f43716df569e3dd535231e1
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 16:35:30 -0600

    Fixed decoding uri

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('main');
 
 app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
-	$scope.video = $sce.trustAsResourceUrl($stateParams.video);
+	$scope.video = decodeURIComponent($stateParams.video);
 	console.log($scope.video);
 });

d1b95785e8dbe1f2783af3f499b86f01e4b1b772
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 17:53:47 -0600

    Moved js code into angular

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -2,4 +2,13 @@ var app = angular.module('main');
 
 app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
 	$scope.video = $sce.trustAsResourceUrl(decodeURIComponent($stateParams.video));
+	console.log(Hls.isSupported());
+	if(Hls.isSupported()) {
+		var video = document.getElementById('video');
+		var hls = new Hls();
+		hls.loadSource($scope.video);
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		video.play();
+	});
 });

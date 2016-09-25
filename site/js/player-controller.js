47755468bce649d6ae6ca893453fb6aa3039cc33
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 17:50:54 -0600

    Testing more ways of getting this done

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -2,5 +2,4 @@ var app = angular.module('main');
 
 app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
 	$scope.video = $sce.trustAsResourceUrl(decodeURIComponent($stateParams.video));
-	console.log($scope.video);
 });

1146076c38db62859b2db3e7abb69fd3346ee4fd
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 16:27:14 -0600

    Fixed url parsing

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('main');
 
-app.controller('PlayerController', function($scope, $log, $stateParams) {
+app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
 	console.log($stateParams);
-	$scope.video = $stateParams.video;
+	$scope.video = $sce.trustAsResourceUrl($stateParams.video);
 });

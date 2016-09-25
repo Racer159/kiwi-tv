731b9aacf94baf6433de2b70f3028a07a1a5c777
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 16:36:29 -0600

    Removed $sce

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('main');
 
-app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
+app.controller('PlayerController', function($scope, $log, $stateParams) {
 	$scope.video = decodeURIComponent($stateParams.video);
 	console.log($scope.video);
 });

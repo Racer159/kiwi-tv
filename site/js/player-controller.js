998685c9b86bb0de681393b7a32315580f0fba57
Wayne Starr <wes7817@g.rit.edu>
2016-09-25 16:28:21 -0600

    Testing parameter reconstruction

--- site/js/player-controller.js
+++ site/js/player-controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('main');
 
 app.controller('PlayerController', function($scope, $log, $stateParams, $sce) {
-	console.log($stateParams);
 	$scope.video = $sce.trustAsResourceUrl($stateParams.video);
+	console.log($scope.video);
 });
